ab90e72fab2629def31072d97d4924e558a41bb6
Troels Korreman <troels.korreman@gmail.com>
2022-01-30 08:51:28 +0100

    Add choice of whether to wrap around when reaching last window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use serde::Deserialize;
-use serde_json::Result;
 use std::env;
 use std::io;
 use std::process::Command;
@@ -25,43 +24,68 @@ struct Tree {
 }
 
 impl Tree {
-    fn focus_idx(&self) -> usize {
-        let (focus_idx, _) = self
-            .nodes
-            .iter()
-            .enumerate()
-            .find(|(_, child)| child.id == self.focus[0])
-            .unwrap();
-        focus_idx
+    fn focus_idx(&self) -> Option<usize> {
+        self.nodes.iter().enumerate().find_map(|(idx, child)| {
+            if child.id == self.focus[0] { Some(idx) } else { None }
+        })
     }
-}
 
-#[derive(Debug, Clone, Copy)]
-enum Direction {
-    Forward,
-    Backward,
+    /// Finds the next or previous node relative to the focused container,
+    /// but only considering the [parent_layout] container type.
+    fn find_neighbor(&self, task: &Task) -> Option<u32> {
+        let mut t = self;
+        let mut target_child = None;
+        while let Some(focus_idx) = t.focus_idx() {
+            if t.focused { break; }
+
+            let num_children = t.nodes.len();
+            if t.layout == task.layout {
+                let branch_idx = focus_idx + num_children;
+                let branch_idx = if task.backward { branch_idx - 1 } else { branch_idx + 1 };
+                let branch_idx = if task.wrap {
+                    branch_idx % num_children
+                } else {
+                    branch_idx.max(num_children).min(num_children * 2 - 1) - num_children
+                };
+
+                target_child = Some(t.nodes.get(branch_idx).unwrap());
+            }
+            t = &t.nodes[focus_idx];
+        }
+        target_child.map(|mut child| {
+            while let Some(focus_idx) = child.focus_idx() {
+                child = &child.nodes[focus_idx];
+            }
+            child.id
+        })
+    }
 }
 
 fn main() {
     let input = io::stdin();
     let input = input.lock();
     let tree: Tree = serde_json::from_reader(input).unwrap();
-    println!("{tree:?}");
-    if let Some((layout, direction)) = parse_args() {
-        if let Some(neighbor) = find_neighbor(&tree, layout, direction) {
+    if let Some(task) = parse_args() {
+        if let Some(neighbor) = tree.find_neighbor(&task) {
             let mut cmd = Command::new("swaymsg");
             cmd.arg(format!("[con_id={neighbor}] focus"));
             cmd.spawn().unwrap().wait().unwrap();
         }
     } else {
-        println!("usage: swaytab (splith|splitv|tabbed|stacked) (forward|backward)");
+        println!("usage: swaytab (splith|splitv|tabbed|stacked) (forward|backward) (wrap|nowrap)");
     }
 }
 
-fn parse_args() -> Option<(Layout, Direction)> {
+struct Task {
+    layout: Layout,
+    backward: bool,
+    wrap: bool,
+}
+
+fn parse_args() -> Option<Task> {
     let args: Vec<String> = env::args().collect();
     match args.len() {
-        3 => {
+        4 => {
             let layout = match args[1].as_str() {
                 "splith" => Some(Layout::SplitH),
                 "splitv" => Some(Layout::SplitV),
@@ -69,39 +93,22 @@ fn parse_args() -> Option<(Layout, Direction)> {
                 "stacked" => Some(Layout::Stacked),
                 _ => None,
             }?;
-            let direction = match args[2].as_str() {
-                "forward" => Some(Direction::Forward),
-                "backward" => Some(Direction::Backward),
+            let backward  = match args[2].as_str() {
+                "backward" => Some(true),
+                "forward" => Some(false),
                 _ => None,
             }?;
-            Some((layout, direction))
+            let wrap = match args[3].as_str() {
+                "wrap" => Some(true),
+                "nowrap" => Some(false),
+                _ => None,
+            }?;
+            Some(Task {
+                layout,
+                backward,
+                wrap,
+            })
         }
         _ => None,
     }
-}
-
-/// Finds the next or previous node relative to the focused container,
-/// but only considering the [parent_layout] container type.
-fn find_neighbor(mut t: &Tree, parent_layout: Layout, dir: Direction) -> Option<u32> {
-    let mut target_child = None;
-    loop {
-        let num_children = t.nodes.len();
-        if num_children == 0 || t.focused {
-            break;
-        }
-        let focus_idx = t.focus_idx();
-        if t.layout == parent_layout {
-            match dir {
-                Direction::Forward => target_child = Some(&t.nodes[focus_idx + 1 % num_children]),
-                Direction::Backward => target_child = Some(&t.nodes[focus_idx - 1 % num_children]),
-            }
-        }
-        t = &t.nodes[focus_idx];
-    }
-    target_child.map(|mut child| {
-        while !child.nodes.is_empty() {
-            child = &child.nodes[child.focus_idx()];
-        }
-        child.id
-    })
 }
